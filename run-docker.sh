3782224e16bba7b5545df12861ae0f6b340bea37
sbortman <scott.bortman@gmail.com>
2020-11-17 15:55:40 -0500

    fixed docker mounts for data

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#docker run -it --rm -p 8080:8080 -v $OSSIM_DATA:$OSSIM_DATA  --entrypoint=sh nexus-docker-public-hosted.ossim.io/ossim-demo:latest 
-docker run -it --rm -p 8080:8080 -v $OSSIM_DATA:$OSSIM_DATA  nexus-docker-public-hosted.ossim.io/ossim-demo:latest 
+#docker run -it --rm -p 8080:8080 -v  $PWD/ossim-demo-data:/ossim-demo-data --entrypoint=sh nexus-docker-public-hosted.ossim.io/ossim-demo:latest 
+docker run -it --rm -p 8080:8080 -v $PWD/ossim-demo-data:/ossim-demo-data  nexus-docker-public-hosted.ossim.io/ossim-demo:latest 
